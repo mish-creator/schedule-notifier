e82138827edee15c31f1de79b71489bc79367af4
dfghj <mishenko9n@yandex.ru>
2026-03-24 05:52:43 +0300

    Добавлены статические файлы для PWA

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  const options = {
+    body: data.body,
+    icon: '/icon-192.png',
+    badge: '/badge.png'
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
